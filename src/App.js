0c4d59d1dcbf1ec611f7179ff080b95ca5931180
bakarytoure <bakarytoure57@gmail.com>
2020-01-25 16:47:48 +0000

    commit 3

--- src/App.js
+++ src/App.js
@@ -6,13 +6,51 @@ import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 //show
 class App extends Component {
+  state = {
+    items: [
+      { id: 1, title: "bakary" },
+      { id: 2, title: "bakary" },
+      { id: 3, title: "bakary" }
+    ],
+    id: uuid(),
+    item: "",
+    editItem: false
+  };
+  handleChange = e => {
+    console.log("handleChange");
+  };
+  handleSubmit = e => {
+    console.log("handleSubmit");
+  };
+  ClearList = () => {
+    console.log("Clear list");
+  };
+  handeDelete = id => {
+    console.log(`handle delete ${id}`);
+  };
+  handeEdit = id => {
+    console.log(`handle edit ${id}`);
+  };
   render() {
+    console.log(this.state);
     return (
       <div>
         <div className="container">
           <div className="row">
-            <TodoInput />
-            <TodoList />
+            <div className="col-10 mx-auto col-md-8 mt-4">
+              <h3 className="text-capitalize text-center">todo input</h3>
+            </div>
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+              editItem={this.editItem}
+            />
+            <TodoList
+              items={this.state.items}
+              ClearList={this.ClearList}
+              handeDelete={this.handeDelete}
+            />
           </div>
         </div>
       </div>
