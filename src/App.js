78a1463b9ad1c0b1e89bbe4f15a776574dcd9bde
bakarytoure <bakarytoure57@gmail.com>
2020-01-25 14:59:07 +0000

    Second commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import uuid from "uuid";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
+//show
 class App extends Component {
   render() {
     return (
